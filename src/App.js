ff57b08b5af6bb9384dbe43b9a07b510da11b9c2
kyurov <karateel@users.noreply.github.com>
2021-09-17 18:13:05 +0300

    useRef()

--- src/App.js
+++ src/App.js
@@ -6,45 +6,50 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 
 function App() {
   const [posts, setPosts] = useState([
-    {id:1, title: 'React 1', body:'Description' },
-    {id:2, title: 'React 2', body:'Description' },
-    {id:3, title: 'React 3', body:'Description' }
+    {id:1, title: 'aaaa', body:'zzz' },
+    {id:2, title: 'ccc', body:'ffff' },
+    {id:3, title: 'bbb', body:'eee' }
   ])
-  const [post, setPost] = useState({title:'', body:''})
+  const [selectedSort, setSelectedSort] = useState('')
 
-
-
-
-  const addNewPost = (e) => {
-    e.preventDefault()
-    setPosts([...posts, {...post, id:Date.now()}])
-    setPost({title:'', body:''})
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
   }
 
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className="App">
-      <form>
-        {/*Управляемый компонент*/}
-        <MyInput
-          value={post.title}
-          onChange = {e => setPost({...post, title:e.target.value})}
-          type="text"
-          placeholder="Title"/>
-
-        {/*Неуправляемый компонент*/}
-          <MyInput
-            value={post.body}
-            onChange = {e => setPost({...post, body:e.target.value})}
-            type="text"
-            placeholder="description"/>
-          <MyButton  onClick={addNewPost}>Create post</MyButton>
-      </form>
-      <PostList posts={posts} title="React Lists"/>
+      <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+       <MySelect
+         value={selectedSort}
+         onChange={sortPosts}
+          defaultValue="Sort by"
+          options={[
+        {value: 'title', name:'title'},
+        {value: 'body', name: 'description'},
+      ]}
+       />
+      </div>
+      {posts.length !== 0
+       ? <PostList remove={removePost} posts={posts} title="React Lists"/>
+        : <h1 style={{textAlign: 'center'}}>NO POSTS HERE</h1>
+      }
     </div>
   );
 }
